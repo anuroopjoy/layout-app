3645156d945f73d907bce72d316634813fcdaabe
Anuroop <anuroopjoy@gmail.com>
2022-01-26 18:44:28 +0530

    initial

---
+++ app-about/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app-about';
+}
