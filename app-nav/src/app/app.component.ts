e6f0f07e93b024bd881d6d199d29bf25693467be
Anuroop <anuroopjoy@gmail.com>
2024-10-31 18:52:24 +0530

    latest

--- app-nav/src/app/app.component.ts
+++ app-nav/src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { singleSpaPropsSubject } from '../single-spa/single-spa-props';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-home';
-  singleSpaProps$ = singleSpaPropsSubject.asObservable();
+  title = 'app-nav';
 }
